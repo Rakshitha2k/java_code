bbfa2351c2772e6d82c18909e1d46acdb243aaf5
rraks <rraks@rakshu>
2026-08-11 20:32:00 +0530

    Session 1

---
+++ javacode/src/javacode/IfElse.java
@@ -0,0 +1,13 @@
+package javacode;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int age=11;
+		if(age>=18)
+			System.out.println("You are eligible for Voting");
+		else
+			System.out.println("Sorry! you need to wait more");
+		System.out.println("if else Demonstration");
+	}
+
+}
